341b16d0feb3770c0895bc89c2939cdf31d73208
Xie Yi <yxie.lhi@gmail.com>
2025-05-20 09:49:34 -0400

    update

--- train.py
+++ train.py
@@ -1,12 +1,25 @@
+#!/usr/bin/python3
+
+from absl import flags, app
 import torch
 from alphafold3_pytorch import Alphafold3, Trainer, create_trainer_from_yaml
 from alphafold3_pytorch.inputs import PDBDataset
 
-# 从 YAML 配置文件创建训练器
-trainer = create_trainer_from_yaml('./tests/configs/trainer_with_pdb_dataset.yaml')
+FLAGS = flags.FLAGS
 
-# 训练模型
+def add_options():
+  flags.DEFINE_bool("weighted_sampling", default = False, help = 'whether weighted sampling')
 
-trainer()
+def main(unused_argv):
+  # 从 YAML 配置文件创建训练器
+  config_path = './tests/configs/trainer_with_pdb_dataset.yaml' if FLAGS.weighted_sampling == False else \
+                './tests/configs/trainer_with_pdb_dataset_and_weighted_sampling.yaml'
+  trainer = create_trainer_from_yaml(config_path)
 
-print("Training finished.")
+  # 训练模型
+  trainer()
+  print("Training finished.")
+
+if __name__ == "__main__":
+  add_options()
+  app.run(main)
